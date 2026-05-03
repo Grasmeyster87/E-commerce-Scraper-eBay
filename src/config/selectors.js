065d5305406140d41166fd695569ae00689d374a
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2026-05-03 21:57:07 +0300

    added selectors

---
+++ src/config/selectors.js
@@ -0,0 +1,7 @@
+export const selectors = {
+    productCard: '.s-item__wrapper',
+    title: '.s-item__title',
+    price: '.s-item__price',
+    link: '.s-item__link',
+    paginationNext: 'a.pagination__next'
+};
